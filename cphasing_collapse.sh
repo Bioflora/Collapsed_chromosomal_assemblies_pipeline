6c4bae3b973db4ab89542902d3b4377481b4cb5a
WenjieUnizar <wmu@unizar.es>
2026-03-14 20:50:13 +0100

    first commit

--- cphasing_collapse.sh
+++ cphasing_collapse.sh
@@ -1,50 +1,66 @@
-#0-1
-cphasing collapse from-gfa ../../../../genome.hic.p_utg.noseq.gfa
-cut -f 1,2 ../genome.hic.p_utg.fa.fai > contig.len.txt
-csvtk merge -T -t -f "1;1" -H contigs.collapsed.contig.list contig.len.txt |perl -anle 'print $_ if ($F[3] > 100000)' | cut -f 1 > z.collapsed.unitig.100k.lst
+## step0: 
+#hifiasm -o genome -t60 --primary -u0 -l0 --h1 hic.fix_1.fastq.gz --h2 hic.fix_2.fastq.gz ccs.fastq.gz
+#hifiasm -o genome -t60 --primary -u0 --h1 hic.fix_1.fastq.gz --h2 hic.fix_2.fastq.gz ccs.fastq.gz
+## step1: first_round hic anchoring, don't need cluster very well, but need to split collapsed region
+cphasing pipeline -f genome.hic.p_ctg.fa -hic1 hic.fix_1.fastq.gz -hic2 hic.fix_2.fastq.gz -t 100 -n 0:0 --pattern GATC
+cd cphasing_output/4.scaffolding; bash to_hic.cmd.sh
+## manually adjust in juice box
+cphasing utils assembly2agp groups.review.assembly -o groups.review
+cphasing utils agp2fasta groups.review.agp ../genome.hic.p_ctg.fa -o groups.review.fasta
+cd ..; mkdir 6.split; cd 6.split; ln -s ../4.scaffolding/groups.review.fasta
+## recommend rename seqence based on homolog
+minimap2 -t 30 -x asm20 --secondary=no ref_g.fa groups.review.fasta > groups.review.fasta.ref_g.paf
+perl Util/paf_stat.alignment_length.pl --paf groups.review.fasta.ref_g.paf --min_length_query 20000 --min_length_match 2000 --pr 0 |sort -k1,1 -k4,4n > groups.review.fasta.ref_g.paf.stat
+perl Util/paf_stat.alignment_length.summary_best.pl groups.review.fasta.ref_g.paf.stat|cut -f 1,2|perl -ple 's/h\d//'|perl -anle 'print "$F[0]\t$F[0]-$F[1]"' > z.rename.lst
+perl Util/fasta_format.rename.pl z.rename.lst groups.review.fasta > groups.review.rename.fasta
 
-#0-2
-minimap2 --secondary=no -x map-hifi -a -t 60 genome.hic.p_utg.fa ccs.fastq.gz | samtools sort -O bam -@ 10 -T align.tmp -o align_long.sort.bam
-perl ~/code/seq/fasta_convert.window_bed.pl --file genome.hic.p_utg.fa --mode bamdst > genome.hic.p_utg.10k.bed
-command_stat.bamdst.pl ./ genome.hic.p_utg.10k.bed |sh; cd align_long
-Rscript ~/code/bamdst/plot.bamdst_depth_distribution.R depth_distribution.plot 0 100
-perl ~/code/bamdst/filter.chromosomes_report.coverage.pl chromosomes.report 0 low 0 |cut -f 1 > z.unmapped.unitig.lst
-perl /home/wenjie/code/seq/fasta_grep.ref.pl -v --ref z.unmapped.unitig.lst ../genome.hic.p_utg.fa > ../genome.hic.p_utg.filter_unmap.fa
+## step2: split 
+perl Util/fasta_split.N.pl --ref groups.review.rename.fasta
+cat groups.review.rename.Nsplit/* > groups.review.rename.split.fasta
+samtools faidx groups.review.rename.fasta
+perl -anle 'print if $F[1] > 5000000' groups.review.rename.fasta.fai |cut -f 1 > groups.review.rename.fasta.chr.lst
+perl Util/cphasing.creat_cluster_table_from_splited_seq.pl groups.review.rename.split.fasta groups.review.rename.fasta.chr.lst > modified.clusters.txt
 
-perl /home/wenjie/code/file/split_file_by_col.pl region.tsv.gz
-perl ~/code/bamdst/stat.depth_region_interval.pl region.tsv.gz.split 15,75 |perl -anle 'print $_ if ($F[4] > 0.3 && $F[5] >= 10)' |cut -f 1 > z.collapsed.unitig.100k.lst
+## step3: detection of collapsed unitig/contig based on mapping depth
+##if you want to recover specific collapsed region, you can just create `z.collapsed.contig.10k.lst` by yourself
+minimap2 --secondary=no -x map-hifi -a -t 120 ./groups.review.rename.split.fasta ccs.fastq.gz | samtools sort -O bam -@ 20 -T align.tmp -o align_long.sort.bam
+perl Util/fasta_convert.window_bed.pl --file groups.review.rename.split.fasta --mode bamdst > groups.review.rename.splitsta.10k.bed
+bamdst -p groups.review.rename.splitsta.10k.bed -o align_long align_long.sort.bam
+command_stat.bamdst.pl ./ groups.review.rename.splitsta.10k.bed |sh;cd align_long
+msplitf region.tsv.gz
+perl Util/stat.depth_region_interval.pl region.tsv.gz.split 30,120 > region.tsv.gz.split.stat
+perl -anle 'print $_ if ($F[4] > 0.2 && $F[5] >= 10)' region.tsv.gz.split.stat |cut -f 1 > z.collapsed.contig.10k.lst
+cd ..
 cd ..
 
-#1
-mkdir z.cphasing
-cd z.cphasing; ln -s ../genome.hic.p_utg.filter_unmap.fa genome.hic.p_utg.fa
-cphasing pipeline -f genome.hic.p_utg.fa -hic1 ~/project/Brachypodium_pangenome/z.raw_data/Bpho6/Bpho6.hic_1.fix.fq.gz -hic2 ~/project/Brachypodium_pangenome/z.raw_data/Bpho6/Bpho6.hic_2.fix.fq.gz -t 100 -n 5:0 -hcr --pattern GATC
-## 5 is the number of homology chromosome group
-cd cphasing_output/4.scaffolding
-bash to_hic.cmd.sh
-
-#2
-#use juicer curate assembly manually
+## step4: re-hic mapping (avoid potential bug when cphasing generate collapse interaction map with splited sequence)
+cphasing pipeline -f groups.review.rename.split.fasta -hic1 hic.fix_1.fastq.gz -hic2 hic.fix_2.fastq.gz -t 100 -n 0:0 --pattern GATC --steps 1,2,3
+mkdir cphasing_output/4.scaffold && cd cphasing_output/4.scaffold
+cphasing scaffolding modified.clusters.txt ../2.prepare/.counts_GATC.txt ../2.prepare/.clm.gz -at ../.allele.table -sc ../2.prepare/.split.contacts -f ../groups.review.rename.split.fasta -t 100 -o groups.agp -m precision;
+cphasing-rs pairs2mnd -q 1 ../.pairs.pqs -o .pqs.mnd.txt
+cphasing utils agp2assembly groups.agp -o groups.assembly
+bash path/to/3d-dna/visualize/run-assembly-visualizer.sh -p true groups.assembly .pqs.mnd.txt
+## manually adjust in juice box
 cphasing utils assembly2agp groups.review.assembly -o groups.review
-cphasing utils agp2fasta groups.review.agp ../genome.hic.p_utg.fa -o groups.review.fasta
-#use `mga groups.review.fasta ref` to check
 
-#3
-perl ~/code/hic/agp.make_duplication.pl ../4.scaffolding/groups.review.agp z.collapsed.unitig.100k.lst contig.len.txt > groups.review.add.agp
+## step5: recover collapsed region and adjust manually
+cd cphasing_output; mkdir 6.collapse; cd 6.collapse
+cut -f 1,2 ../groups.review.rename.split.fasta.fai > contig.len.txt
+ln -s path/to/z.collapsed.contig.10k.lst ./
+perl Util/hic/agp.make_duplication.pl ../4.scaffolding/groups.review.agp z.collapsed.unitig.10k.lst contig.len.txt > groups.review.add.agp
 cphasing collapse agp-dup groups.review.add.agp -o groups.review.add.dup.agp
 #perl ~/code/hic/agp.re_check_cphasing_collapse_agp-dup.pl groups.review.add.dup.agp z.collapsed.unitig.10k.lst > groups.review.add.dup.agp.fix
 #mv groups.review.add.dup.agp.fix groups.review.add.dup.agp
-grep 'd2' groups.review.add.dup.agp |cut -f 6 |perl -nle '$t = $_; $t =~ s/\_d2//;print "$t\t$_"' > duplicated.contigs.txt
-cphasing collapse pairs-dup ../Bpho6.hic.fix.pairs.pqs duplicated.contigs.txt -o ../Bpho6.hic.fix.pairs.collapse.pqs
-cphasing-rs pairs2mnd -q 1 ../Bpho6.hic.fix.pairs.collapse.pqs -o Bpho6.hic.fix.pqs.collapse.mnd.txt
+#grep 'd2' groups.review.add.dup.agp |cut -f 6 |perl -nle '$t = $_; $t =~ s/\_d2//;print "$t\t$_"' > duplicated.contigs.txt
+grep '_d' groups.review.add.dup.agp |cut -f 6 |perl -nle '$t = $_; $t =~ s/\_d\d+//;print "$t\t$_"' > duplicated.contigs.txt
+cphasing collapse pairs-dup ../genome.hic.fix.pairs.pqs duplicated.contigs.txt -o ../genome.hic.fix.pairs.collapse.pqs
+cphasing-rs pairs2mnd -q 1 ../genome.hic.fix.pairs.collapse.pqs -o genome.hic.fix.pqs.collapse.mnd.txt
 cphasing utils agp2assembly groups.review.add.dup.agp -o groups.review.add.dup.assembly
-bash ~/software/3d-dna/visualize/run-assembly-visualizer.sh -p true groups.review.add.dup.assembly Bpho6.hic.fix.pqs.collapse.mnd.txt
+bash path/to/3d-dna/visualize/run-assembly-visualizer.sh -p true groups.review.add.dup.assembly genome.hic.fix.pqs.collapse.mnd.txt
+# manually adjust in juice box #
 
-#4
-#use juicer curate assembly manually
-cphasing utils assembly2agp groups.review.add.dup.review.final.assembly -o groups.review.add.dup.review.final
-ln -s ../6.collapse/duplicated.contigs.txt ./
-perl /home/wenjie/code/seq/fasta_format.make_dup.pl duplicated.contigs.txt ../genome.hic.p_utg.fa > genome.hic.p_utg.dup.fa
-cphasing utils agp2fasta groups.review.add.dup.review.final.agp genome.hic.p_utg.dup.fa -o groups.review.final.fasta
-#use `mga xxxx.fa ref` to check
+## step6: get finally genome assembly
+cphasing utils assembly2agp groups.review.add.dup.review.assembly -o groups.review.add.dup.review
+perl Util/fasta_format.make_dup.pl duplicated.contigs.txt ../groups.review.rename.split.fasta > groups.review.rename.split.dup.fasta
+cphasing utils agp2fasta groups.review.add.dup.review.agp groups.review.rename.split.dup.fasta -o groups.review.add.dup.review.fasta
 
