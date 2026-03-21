919e8709646dad2b65268f32915ed8ebc3b57992
WenjieUnizar <wmu@posta.unizar.es>
2026-03-21 11:44:47 +0100

    Update cphasing_collapse.sh

--- cphasing_collapse.sh
+++ cphasing_collapse.sh
@@ -22,14 +22,16 @@ perl -anle 'print if $F[1] > 5000000' groups.review.rename.fasta.fai |cut -f 1 >
 perl Util/cphasing.creat_cluster_table_from_splited_seq.pl groups.review.rename.split.fasta groups.review.rename.fasta.chr.lst > modified.clusters.txt
 
 ## step3: detection of collapsed unitig/contig based on mapping depth
-##if you want to recover specific collapsed region, you can just create `z.collapsed.contig.10k.lst` by yourself
+# if you want to recover specific collapsed region, you can just create `z.collapsed.contig.10k.lst` by yourself
 minimap2 --secondary=no -x map-hifi -a -t 120 ./groups.review.rename.split.fasta ccs.fastq.gz | samtools sort -O bam -@ 20 -T align.tmp -o align_long.sort.bam
 perl Util/fasta_convert.window_bed.pl --file groups.review.rename.split.fasta --mode bamdst > groups.review.rename.splitsta.10k.bed
 cd align_long
 bamdst -p groups.review.rename.splitsta.10k.bed -o align_long align_long.sort.bam
 perl Util/split.col.pl region.tsv.gz
+# here if you just put one number e.g. 40, which means split depth in three classes 0, 0-40 and > 40 
 perl Util/stat.depth_region_interval.pl region.tsv.gz.split 30,120 > region.tsv.gz.split.stat
-perl -anle 'print $_ if ($F[4] > 0.2 && $F[5] >= 10)' region.tsv.gz.split.stat |cut -f 1 > z.collapsed.contig.10k.lst
+# $F[-1] > 10 indicate only capture utg > 100000 bp
+perl -anle 'print $_ if ($F[3] > 0.2 && $F[-1] >= 10)' region.tsv.gz.split.stat |cut -f 1 > z.collapsed.contig.100k.lst
 cd ..
 mv cphasing_output cphasing_output_0
 cd ..
@@ -51,9 +53,6 @@ cut -f 1,2 ../groups.review.rename.split.fasta.fai > contig.len.txt
 ln -s path/to/z.collapsed.contig.10k.lst ./
 perl Util/hic/agp.make_duplication.pl ../4.scaffold/groups.review.agp z.collapsed.unitig.10k.lst contig.len.txt > groups.review.add.agp
 cphasing collapse agp-dup groups.review.add.agp -o groups.review.add.dup.agp
-#perl ~/code/hic/agp.re_check_cphasing_collapse_agp-dup.pl groups.review.add.dup.agp z.collapsed.unitig.10k.lst > groups.review.add.dup.agp.fix
-#mv groups.review.add.dup.agp.fix groups.review.add.dup.agp
-#grep 'd2' groups.review.add.dup.agp |cut -f 6 |perl -nle '$t = $_; $t =~ s/\_d2//;print "$t\t$_"' > duplicated.contigs.txt
 grep '_d' groups.review.add.dup.agp |cut -f 6 |perl -nle '$t = $_; $t =~ s/\_d\d+//;print "$t\t$_"' > duplicated.contigs.txt
 cphasing collapse pairs-dup ../hic.fix.pairs.pqs duplicated.contigs.txt -o ../genome.hic.fix.pairs.collapse.pqs
 cphasing-rs pairs2mnd -q 1 ../hic.fix.pairs.collapse.pqs -o genome.hic.fix.pqs.collapse.mnd.txt
